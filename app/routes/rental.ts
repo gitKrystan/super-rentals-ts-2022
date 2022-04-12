b538ac0fd7c91f85a0063d78d0d610b195d5e523
Krystan HuffMenne <krystan@tilde.io>
2022-04-12 14:12:35 -0700

    Fix Rental route TS errors

--- app/routes/rental.ts
+++ app/routes/rental.ts
@@ -1,10 +1,19 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import Store from '@ember-data/store';
+
+// eslint-disable-next-line ember/use-ember-data-rfc-395-imports
+import DS from 'ember-data';
+import RentalModel from 'super-rentals/models/rental';
+
+interface RentalRouteParams {
+  rental_id: string;
+}
 
 export default class RentalRoute extends Route {
-  @service store;
+  @service declare store: Store;
 
-  async model(params) {
+  model(params: RentalRouteParams): DS.PromiseObject<RentalModel> {
     return this.store.findRecord('rental', params.rental_id);
   }
 }

d4e601b68decc7063816baee2b7e52ac6aff11f4
Krystan HuffMenne <krystan@tilde.io>
2022-04-12 13:58:08 -0700

    Fix Rental model TS errors

--- app/models/rental.ts
+++ app/models/rental.ts
@@ -2,17 +2,22 @@ import Model, { attr } from '@ember-data/model';
 
 const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
 
-export default class RentalModel extends Model {
-  @attr title;
-  @attr owner;
-  @attr city;
-  @attr location;
-  @attr category;
-  @attr image;
-  @attr bedrooms;
-  @attr description;
+interface MapLocation {
+  lat: number;
+  lng: number;
+}
 
-  get type() {
+export default class RentalModel extends Model {
+  @attr declare title: string;
+  @attr declare owner: string;
+  @attr declare city: string;
+  @attr declare location: MapLocation;
+  @attr declare category: string;
+  @attr declare image: string;
+  @attr declare bedrooms: number;
+  @attr declare description: string;
+
+  get type(): 'Community' | 'Standalone' {
     if (COMMUNITY_CATEGORIES.includes(this.category)) {
       return 'Community';
     } else {

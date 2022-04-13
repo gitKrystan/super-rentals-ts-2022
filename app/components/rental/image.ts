61203e7f08d8bb69c9d4558fb6f8859838c41cae
Krystan HuffMenne <krystan@tilde.io>
2022-04-13 16:18:16 -0700

    Fix Rental::Image glint errors

--- app/components/rental/image.ts
+++ app/components/rental/image.ts
@@ -2,7 +2,11 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
-export default class RentalImageComponent extends Component {
+interface RentalImageSignature {
+  Element: HTMLImageElement;
+}
+
+export default class RentalImageComponent extends Component<RentalImageSignature> {
   @tracked isLarge = false;
 
   @action toggleSize(): void {

318831fef6cec0839c5794c083325c86bcb3ad2f
Krystan HuffMenne <krystan@tilde.io>
2022-04-13 16:47:16 -0700

    Fix Rentals component glint errors

--- app/components/rentals.ts
+++ app/components/rentals.ts
@@ -1,6 +1,14 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import IndexRoute from 'super-rentals/routes/index';
+import { ModelFrom } from 'super-rentals/types/util';
 
-export default class RentalsComponent extends Component {
+interface RentalsSignature {
+  Args: {
+    rentals: ModelFrom<IndexRoute>;
+  };
+}
+
+export default class RentalsComponent extends Component<RentalsSignature> {
   @tracked query = '';
 }

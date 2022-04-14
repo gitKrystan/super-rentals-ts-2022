3db7fd9734a503dcc054bd8eaee24afacc01843d
Krystan HuffMenne <krystan@tilde.io>
2022-04-13 18:42:40 -0700

    Fix lint

--- types/super-rentals/index.d.ts
+++ types/super-rentals/index.d.ts
@@ -1,13 +1,6 @@
-import Ember from 'ember';
-
 import '@glint/environment-ember-loose';
 
 // NOTE: This import won't be necessary after Glint 0.8
 import '@glint/environment-ember-loose/native-integration';
-
-declare global {
-  interface Array<T> extends Ember.ArrayPrototypeExtensions<T> {}
-  // interface Function extends Ember.FunctionPrototypeExtensions {}
-}
 
 export {};

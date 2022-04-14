1e3946f7726cfe7ae5fb253436ead53f9500e96f
Krystan HuffMenne <krystan@tilde.io>
2022-04-14 11:14:48 -0700

    Clean up Rental route types

--- app/routes/rental.ts
+++ app/routes/rental.ts
@@ -1,9 +1,6 @@
+import Store from '@ember-data/store';
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
-import Store from '@ember-data/store';
-
-// eslint-disable-next-line ember/use-ember-data-rfc-395-imports
-import DS from 'ember-data';
 import RentalModel from 'super-rentals/models/rental';
 
 interface RentalRouteParams {
@@ -13,7 +10,7 @@ interface RentalRouteParams {
 export default class RentalRoute extends Route {
   @service declare store: Store;
 
-  model(params: RentalRouteParams): DS.PromiseObject<RentalModel> {
-    return this.store.findRecord('rental', params.rental_id);
+  async model(params: RentalRouteParams): Promise<RentalModel> {
+    return await this.store.findRecord('rental', params.rental_id);
   }
 }
